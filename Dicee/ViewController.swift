66978deef7a0b628ee3ad64f9d23751421db5758
NandaMochammad <nanda.mocha@gmail.com>
2019-04-04 13:20:22 +0700

    Initial Commit

---
+++ Dicee/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Dicee
+//
+//  Created by Nanda Mochammad on 04/04/19.
+//  Copyright © 2019 Nanda Mochammad. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
